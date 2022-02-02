b1dee7503a1de7b642623a9dfaf01c93c3e3454b
Eduardo Martínez <emartinezr161@outlook.com>
2022-02-02 19:44:00 +0100

    comenzado dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,32 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-3xl bg-white border-b border-gray-200">
+                    Bienvenido, {{ Auth::user()->name }}.
+                </div>
+            </div>
+        </div>
+        <div class="py-2 max-w-7xl mx-auto sm:px-6 lg:px-8 text-xl">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <a class="px-4 py-2 hover:underline" href="{{ route('ver-citas') }}">Ver citas</a>
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <a class="px-4 py-2 hover:underline" href="{{ route('ver-citas') }}">Crear una cita</a>
+                </div>
+                {{-- <div class="p-6 bg-gray-200 border-b border-gray-500">
+                    <a class="px-4 py-2 rounded bg-green-" href="{{ route('crear-cita-compania') }}">Nueva cita</a>
+                </div> --}}
+            </div>
+        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-4 bg-white border-b border-gray-200 text-2xl">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @method('POST')
+                        @csrf
+                        <button class="px-4 py-2 hover:underline" type="submit">Salir</button>
+                    </form>
                 </div>
             </div>
         </div>
